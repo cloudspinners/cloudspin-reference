c94069a11810209928517c575b1c901717419fb6
kief <kmorris@thoughtworks.com>
2018-09-10 12:11:07 +0100

    Check that a stack instance attribute is made available

--- part1/examples/01-basic-stack/test/inspec/controls/vpc.rb
+++ part1/examples/01-basic-stack/test/inspec/controls/vpc.rb
@@ -7,3 +7,10 @@ stack_instance_id = attribute('stack_instance_id', description: 'Which stack to 
 describe aws_vpc_list do
   its('name') { should include "vpc-#{stack_instance_id}" }
 end
+
+region = attribute('region', description: 'aws region')
+describe region do
+  it 'region attribute should be available' do
+    subject.should_not eq('')
+  end
+end
